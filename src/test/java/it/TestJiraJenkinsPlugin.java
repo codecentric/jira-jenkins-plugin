df50f76cfe345076a61d680431dd6eef5117beba
Philipp Rothe <p.rothe@gmx.at>
2012-02-07 12:57:36 +0100

    leerer Integrationstest hinzugefuegt

---
+++ src/test/java/it/TestJiraJenkinsPlugin.java
@@ -0,0 +1,16 @@
+package it;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.atlassian.jira.functest.framework.FuncTestCase;
+
+public class TestJiraJenkinsPlugin extends FuncTestCase {
+	@Before
+	public void setUpTest(){
+	}
+	
+	@Test
+    public void integrationTest(){
+    }
+}
